f1bc06e0e05b1640bd5daf07d6da1747104a479c
Lucas Pina <ti.lucaspina@gmail.com>
2025-01-19 12:36:50 -0300

    versão funcional: implementação dos itens propostos no desafio

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+  let amigo = document.getElementById("amigo").value;
+
+  if (amigo === "") {
+    alert("Por favor, insira um nome.");
+  } else {
+    amigos.push(amigo);
+    document.getElementById("amigo").value = "";
+    atualizarListaAmigos();
+  }
+}
+
+function limparListaNomes(id) {
+  let lista = document.getElementById(id);
+  lista.innerHTML = "";
+}
+
+function atualizarListaAmigos() {
+  let lista = document.getElementById("listaAmigos");
+  limparListaNomes("listaAmigos");
+
+  for (let i = 0; i < amigos.length; i++) {
+    lista.innerHTML += `<li>${amigos[i]}</li>`;
+  }
+}
+
+function amigoAleatorio() {
+  let quantidadeParaSortear = amigos.length;
+  return Math.floor(Math.random() * quantidadeParaSortear);
+}
+
+function sortearAmigo() {
+  let listaResultado = document.getElementById("resultado");
+  if (amigos.length > 0) {
+    limparListaNomes("listaAmigos");
+
+    let numeroSorteado = amigoAleatorio();
+    let amigoSorteado = amigos[numeroSorteado];
+
+    listaResultado.innerHTML = `O amigo sorteado é: ${amigoSorteado}`;
+  } else {
+    alert("Não tem ninguém para sortear, adicione nomes a lista!");
+  }
+}
